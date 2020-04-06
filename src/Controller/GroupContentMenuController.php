09306ab296f631caa7d647dc7c80f0c90d840070
lucashedding <lucashedding@1463982.no-reply.drupal.org>
2020-04-06 16:30:25 -0600

    Issue #3118939 by heddn, ankithashetty: Drupal 9 Deprecated Code Report

--- src/Controller/GroupContentMenuController.php
+++ src/Controller/GroupContentMenuController.php
@@ -2,16 +2,11 @@
 
 namespace Drupal\group_content_menu\Controller;
 
-use Drupal\Core\Entity\EntityFormBuilderInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Link;
-use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Url;
 use Drupal\group\Entity\Controller\GroupContentController;
 use Drupal\group\Entity\GroupInterface;
-use Drupal\group\Plugin\GroupContentEnablerManagerInterface;
 use Drupal\group_content_menu\GroupContentMenuInterface;
-use Drupal\user\PrivateTempStoreFactory;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -28,35 +23,11 @@ class GroupContentMenuController extends GroupContentController {
   protected $pluginManager;
 
   /**
-   * Constructs a new GroupContentMenuController.
-   *
-   * @param \Drupal\group\Plugin\GroupContentEnablerManagerInterface $plugin_manager
-   *   The group content plugin manager.
-   * @param \Drupal\user\PrivateTempStoreFactory $temp_store_factory
-   *   The private store factory.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Entity\EntityFormBuilderInterface $entity_form_builder
-   *   The entity form builder.
-   * @param \Drupal\Core\Render\RendererInterface $renderer
-   *   The renderer.
-   */
-  public function __construct(GroupContentEnablerManagerInterface $plugin_manager, PrivateTempStoreFactory $temp_store_factory, EntityTypeManagerInterface $entity_type_manager, EntityFormBuilderInterface $entity_form_builder, RendererInterface $renderer) {
-    parent::__construct($temp_store_factory, $entity_type_manager, $entity_form_builder, $renderer);
-    $this->pluginManager = $plugin_manager;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('plugin.manager.group_content_enabler'),
-      $container->get('user.private_tempstore'),
-      $container->get('entity_type.manager'),
-      $container->get('entity.form_builder'),
-      $container->get('renderer')
-    );
+    $instance = parent::create($container);
+    $instance->privateTempStoreFactory = $container->get('tempstore.private');
   }
 
   /**
@@ -141,7 +112,6 @@ class GroupContentMenuController extends GroupContentController {
         return new RedirectResponse($url->toString());
       }
     }
-    return FALSE;
   }
 
   /**
